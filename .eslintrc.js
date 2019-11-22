406bc2b22ff5cee1243ebd1244a77b5ce9265620
Manoel Malafaia <mmalafaia@gmail.com>
2019-11-22 16:11:18 -0300

    Commit: State using Hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,30 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'airbnb',
-        'prettier',
-        'prettier/react'
-    ],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly'
-    },
-    parser: 'babel-eslint',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        ecmaVersion: 2018,
-        sourceType: 'module'
-    },
-    plugins: [
-        'react',
-        'prettier'
-    ],
-    rules: {
-      'prettier/prettier': 'error',
-      'react/jsx-filename-extension': [
-        'warn',
-        { extensions: ['.jsx', '.js'] }
-      ],
-      'import/prefer-default-export': 'off',
-      'react/state-in-constructor': 'off',
-      'no-param-reassing': 'off',
-      'no-console': ['error', { allow: ['tron'] }],
-    }
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    'no-param-reassing': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/state-in-constructor': 'off',
+  },
 };
